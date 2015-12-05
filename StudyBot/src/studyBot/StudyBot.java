87f4f9a6c0cbf213ad64cf2dbd6e1ee3143fd362
Jorge Sanz <jorsanzp@gmail.com>
2015-12-05 10:41:13 +0100

    Add Hello World functionality

--- StudyBot/src/studyBot/StudyBot.java
+++ StudyBot/src/studyBot/StudyBot.java
@@ -1,8 +1,22 @@
 package studyBot;
 
+import telegramBotAPI.*;
+
 public class StudyBot {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ApiKey botKey = new ApiKey();
+		TelegramBot bot = new TelegramBot(botKey.getApiKey());
+		while(!bot.throwMessages()){}
+		TelegramMsg msg;
+		while(true){
+			while((msg = bot.nextMsg(10)) == null){}
+			while(!bot.sendText("Hi, my name is @StudyBot! I'm working to "
+					+ "improve my skills in order to enrich your student group "
+					+ "chat more. Have you got any idea about what I could do? "
+					+ "Send it to @jorgesanz. He will be thankful! :-)",
+					msg.getChat())){}
+		}
 	}
 }
